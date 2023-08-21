b4c2db994b8c5eb28d42515deb96db0408e175d2
gongym <1967988842@qq.com>
2023-08-21 12:18:44 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module gopkg-gorm
+
+go 1.21
+
+require (
+	github.com/ggymm/gopkg v1.0.5
+	github.com/pkg/errors v0.9.1
+	github.com/rs/zerolog v1.30.0
+	gorm.io/gorm v1.25.4
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+)
+
+replace github.com/ggymm/gopkg => C:\Product\gopkg\gopkg
---
+++ log/logger.go
@@ -0,0 +1,66 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/utils"
+
+	"github.com/ggymm/gopkg/log"
+)
+
+type CustomLog struct {
+	log      zerolog.Logger
+	LogLevel logger.LogLevel
+}
+
+func NewCustomLog(filename string) *CustomLog {
+	return &CustomLog{
+		log:      log.InitCustom(filename),
+		LogLevel: logger.Info,
+	}
+}
+
+func (l *CustomLog) LogMode(level logger.LogLevel) logger.Interface {
+	l.LogLevel = level
+	return l
+}
+
+func (l *CustomLog) Info(_ context.Context, msg string, data ...interface{}) {
+	l.log.Info().Str("file", utils.FileWithLineNum()).Msgf(msg, data...)
+}
+
+func (l *CustomLog) Warn(_ context.Context, msg string, data ...interface{}) {
+	l.log.Warn().Str("file", utils.FileWithLineNum()).Msgf(msg, data...)
+}
+
+func (l *CustomLog) Error(_ context.Context, msg string, data ...interface{}) {
+	l.log.Error().Str("file", utils.FileWithLineNum()).Msgf(msg, data...)
+}
+
+// Trace print sql message
+func (l *CustomLog) Trace(_ context.Context, begin time.Time, fc func() (string, int64), err error) {
+	elapsed := time.Since(begin)
+	costTime := fmt.Sprintf("%.3fms", float64(elapsed.Nanoseconds())/1e6)
+
+	file := utils.FileWithLineNum()
+
+	sql, rows := fc()
+	sql = strings.Replace(sql, "\"", "'", -1)
+
+	if err != nil {
+		// 忽略记录不存在的错误
+		if !errors.Is(err, logger.ErrRecordNotFound) {
+			l.log.Error().Err(errors.WithStack(err)).Str("sql", sql).Str("file", file).
+				Str("costTime", costTime).Int64("rowsAffected", rows).Msg("SQLTrace")
+		}
+	} else {
+		l.log.Info().Str("sql", sql).Str("file", file).
+			Str("costTime", costTime).Int64("rowsAffected", rows).Msg("SQLTrace")
+	}
+}
